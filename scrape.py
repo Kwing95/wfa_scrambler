1699042ad66a431a3f70964a2c4d0d950a684f86
Kwing95 <wangkev@DESKTOP-14FKAUP.localdomain>
2019-09-07 17:01:28 -0500

    build JSON thesaurus from dictionary

--- scrape.py
+++ scrape.py
@@ -1,16 +1,29 @@
+import requests
 import http.client
 from lxml import html
 
+def get_synonyms(conn, word):
+    conn.request("GET", "/browse/" + word)
+    r1 = conn.getresponse()
+    data1 = r1.read()
+    if(len(data1.decode("utf-8")) == 0):
+        return []
+    tree = html.fromstring(data1)
+    synonyms = tree.xpath('//a[@class="css-gkae64 etbu2a31"]/text()')
+    return synonyms
+
+dictionary = open("words_alpha.txt", "r")
+thesaurus = dict()
+url = "https://www.thesaurus.com/browse/"
 conn = http.client.HTTPSConnection("www.thesaurus.com")
-conn.request("GET", "/browse/honesty")
-r1 = conn.getresponse()
-data1 = r1.read()
 
-# print(data1)
-# raw html
+print("honesty = ")
+k = get_synonyms(conn, "honesty")
 
+word = dictionary.readline().strip('\n')
+while(word != ""):
+        thesaurus[word] = get_synonyms(conn, word)
+        print(thesaurus)
+        word = dictionary.readline().strip('\n')
 
-tree = html.fromstring(data1)
-
-synonyms = tree.xpath('//a[@class="css-3kshty etbu2a31"]/text()')
-print(synonyms)
+print(thesaurus)
